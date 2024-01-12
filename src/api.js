f54d2f9802e92dca3833844514665950535f9cfe
Vinod KR <109779300+vin-dKR@users.noreply.github.com>
2024-01-12 17:38:07 +0530

    Create api.js

---
+++ src/api.js
@@ -0,0 +1,13 @@
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = 'd3495700a11bd62eeddb143610e7dc4d';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8b1f222cf2msh838fcc85a58be9cp16e9a0jsnbe9bcf0a9d05',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
