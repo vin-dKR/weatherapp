1649f96ec55cbe281d5889654cf585b857ea3e60
Vinod KR <109779300+vin-dKR@users.noreply.github.com>
2024-01-12 17:32:52 +0530

    Update App.js
    added more states

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { WEATHER_API_KEY, WEATHER_API_URL } from "./api";
+import "./App.css";
+import CurrentWeather from "./components/Current-weather";
+import Search from "./components/search/search";
+import Forcast from "./components/Forcast";
+import "./components/responsive.css";
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [foreCast, setForeCast] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split("");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const foreCastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, foreCastFetch])
+    .then(async (response) => {
+      const weatherResponse = await response[0].json();
+      const forecastResponse = await response[1].json();
+
+      setCurrentWeather({ city: searchData.label, ...weatherResponse });
+      setForeCast({ city: searchData.label, ...forecastResponse });
+    })
+    .catch((err) =>{
+      console.log(err);
+    })
+  };
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* //on typing input it loading... */}
+      <Search onSearchChange={handleOnSearchChange} className="search" />
+
+      {/* currentWeather Card */}
+      {currentWeather && <CurrentWeather data={currentWeather} className="current-weather" />}
+
+      {/* weatherForecast Details */}
+      {foreCast && <Forcast data={foreCast} className="weather-forecast" />}
     </div>
   );
 }
